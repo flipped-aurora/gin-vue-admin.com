c8f3b3cfea548d2b8b4350cfb3da0d80b04c1e1f
pixelMax(奇淼 <flipped_aurora@163.com>
2024-05-10 11:31:51 +0800

    授权费优惠期结束预告

--- docs/.vitepress/components/EmPower.vue
+++ docs/.vitepress/components/EmPower.vue
@@ -1,4 +1,18 @@
   <template>
+    <div class="flex items-center gap-x-6 bg-gray-900 px-6 py-2.5 sm:px-3.5 sm:before:flex-1" @click="toLink">
+      <p class="text-sm leading-6 text-white">
+                <span class="cursor-pointer">
+                    <strong class="font-semibold">友情提示：</strong>
+                  优惠授权费用将于6月30日结束，之后将恢复原价。若您想享受优惠，请在此之前联系我们购买授权。<span
+                    aria-hidden="true">&rarr;</span>
+                </span>
+      </p>
+      <div class="flex flex-1 justify-end">
+        <button type="button" class="-m-3 p-3 focus-visible:outline-offset-[-4px]">
+          <span class="sr-only">Dismiss</span>
+        </button>
+      </div>
+    </div>
     <div class="max-w-7xl mx-auto py-24 px-4 bg-white sm:px-6 lg:px-8">
       <h2 class="text-3xl font-extrabold text-gray-900 sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl">购买授权</h2>
       <p class="mt-6 max-w-2xl text-xl text-gray-500">如果您需要去除系统下方的Gin-Vue-Admin以及flipped-aurora技术支持标识，这表明您打算将项目用于商业目的。根据授权协议，您必须获得相应的授权。项目内置了隐蔽的指纹识别功能，您需获取授权文件以去除指纹标记。</p>
@@ -34,12 +48,15 @@
 
   <script setup>
     import {ref} from "vue"
+    const toLink = () => {
+      window.open('https://flipped-aurora.feishu.cn/docx/KwjxdnvatozgwIxGV0rcpkZSn4d?from=from_copylink')
+    }
     const pricing = ref({
       tiers: [
         {
           title: '企业自用版授权',
           price: 888,
-          nextPrice: 1088,
+          nextPrice: 1288,
           frequency: '终生',
           description: '此授权您可以剔除系统下方的Gin-Vue-Admin以及flipped-aurora技术支持标识，但在本授权模式下，您所开发的软件不可以做为项目进行对外出售，仅用于本公司运营项目使用。',
           features: ['可以剔除版权标识', '专属交互式文档', '专属企业飞书群', '及时的团队答疑','插件享受企业版优惠价格'],
