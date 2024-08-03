8d7bac61d69c6185ed7c9b36c8752d2f856c5745
bypanghu <bypanghu@163.com>
2024-08-03 10:30:09 +0800

    fix: 增加 授权页面提示

--- docs/.vitepress/components/EmPower.vue
+++ docs/.vitepress/components/EmPower.vue
@@ -1,23 +1,23 @@
   <template>
-<!--    <div class="flex items-center gap-x-6 bg-gray-900 px-6 py-2.5 sm:px-3.5 sm:before:flex-1" @click="toLink">-->
-<!--      <p class="text-sm leading-6 text-white">-->
-<!--                <span class="cursor-pointer">-->
-<!--                    <strong class="font-semibold">友情提示：</strong>-->
-<!--                  优惠授权费用将于6月30日结束，之后将恢复原价。若您想享受优惠，请在此之前联系我们购买授权。<span aria-hidden="true">&rarr;</span>-->
-<!--                </span>-->
-<!--      </p>-->
-<!--      <div class="flex flex-1 justify-end">-->
-<!--        <button type="button" class="-m-3 p-3 focus-visible:outline-offset-[-4px]">-->
-<!--          <span class="sr-only">Dismiss</span>-->
-<!--        </button>-->
-<!--      </div>-->
-<!--    </div>-->
-    <div class="max-w-7xl mx-auto py-24 px-4 bg-white sm:px-6 lg:px-8">
+    <div class="flex items-center gap-x-6 bg-orange-500 px-6 py-2.5 sm:px-3.5 sm:before:flex-1 " @click="">
+      <p class="text-sm leading-6 text-white max-w-7xl">
+        <span class="cursor-pointer">
+          gin-vue-admin 再此承诺，项目除必须联网的   <span>AI服务</span>， <span>部分`插件`</span> 外，不会主动请求任何外部gva服务。
+如后期增加主动查询授权协议等功能，会在官网和 github 提前通知，告知所有用户（目前内部暂无此计划)
+        </span>
+      </p>
+      <div class="flex flex-1 justify-end">
+        <button type="button" class="-m-3 p-3 focus-visible:outline-offset-[-4px]">
+          <span class="sr-only">Dismiss</span>
+        </button>
+      </div>
+    </div>
+    <div class="max-w-7xl mx-auto py-24 px-4 bg-white sm:px-6 lg:px-0">
       <h2 class="text-3xl font-extrabold text-gray-900 sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl">购买授权</h2>
       <p class="mt-6 max-w-2xl text-xl text-gray-500">如果您需要去除系统下方的Gin-Vue-Admin以及flipped-aurora技术支持标识，这表明您打算将项目用于商业目的。根据授权协议，您必须获得相应的授权。项目内置了隐蔽的指纹识别功能【无安全隐患】，获取授权文件后在官方指导下去除指纹标记。</p>
       <!-- Tiers -->
       <div class="mt-12 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-8">
-        <div v-for="tier in pricing.tiers" :key="tier.title" class="relative p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col">
+        <div v-for="tier in pricing.tiers" :key="tier.title" class="relative p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col group cursor-pointer">
           <div class="flex-1">
             <h3 class="text-xl font-semibold text-gray-900">{{ tier.title }}</h3>
             <p v-if="tier.mostPopular" class="absolute top-0 py-1.5 px-4 bg-indigo-500 rounded-full text-xs font-semibold uppercase tracking-wide text-white transform -translate-y-1/2">最多购买</p>
@@ -38,7 +38,7 @@
               </li>
             </ul>
           </div>
-          <a :href="tier.link" target="_blank"  :class="[tier.mostPopular ? 'bg-indigo-500 text-white hover:bg-indigo-600' : 'bg-indigo-50 text-indigo-700 hover:bg-indigo-100', 'mt-8 block w-full py-3 px-6 border border-transparent rounded-md text-center font-medium']">{{ tier.cta }}</a>
+          <a :href="tier.link" target="_blank"  :class="[tier.mostPopular ? 'bg-indigo-500 text-white hover:bg-indigo-600' : 'bg-indigo-50 text-indigo-700 hover:bg-indigo-100', 'mt-8 block w-full py-3 px-6 border border-transparent rounded-md text-center font-medium group-hover:-translate-y-1 ease-in-out ']">{{ tier.cta }}</a>
         </div>
       </div>
     </div>
