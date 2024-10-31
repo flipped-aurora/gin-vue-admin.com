548a76b28fc3fd50e4f3f2acb51cc2f6802f5a8b
bypanghu <bypanghu@163.com>
2024-10-31 11:54:01 +0800

    fix:首页添加 ai 文字

--- docs/.vitepress/theme/components/liuliang.vue
+++ docs/.vitepress/theme/components/liuliang.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed bottom-10 right-0 gap-2 flex flex-col z-auto  bg-white shadow p-2 rounded" v-if="showAD">
+  <div class="fixed bottom-10 right-0 gap-2 flex flex-col z-[999]  bg-white shadow p-2 rounded" v-if="showAD">
     <a class="flex items-center justify-center rounded overflow-hidden" target="_blank" href="https://plugin.gin-vue-admin.com/#/layout/newPluginInfo?id=69">
       <img :width="160" src="/advertising/cms.png" alt="cms">
     </a>
