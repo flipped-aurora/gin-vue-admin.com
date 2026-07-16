21c2d2b67cef986de312aa71ffd059080e7bed4d
PiexlMax(奇淼 <flipped_aurora@163.com>
2026-07-16 20:21:13 +0800

    feat: 更新首页样式，添加首页标记，调整顶栏背景颜色

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -368,11 +368,11 @@ export default defineConfig({
                             link: '/guide/video/web'
                         },
                     ]
-                },
-                {
-                    text: '🔥特惠服务器',
-                    link: 'https://flipped-aurora.feishu.cn/wiki/HNnPwI9TbifCvwk51A2cLqOjnac?from=from_copylink'
                 }
+                // {
+                //     text: '🔥特惠服务器',
+                //     link: 'https://flipped-aurora.feishu.cn/wiki/HNnPwI9TbifCvwk51A2cLqOjnac?from=from_copylink'
+                // }
             ],
             '/experience/': [
                 {
